03fe0afca4b64253e4a62cbd7c84f27a485ec3f4
Ovidiu Calbajos <ovidiu.calbajos@gmail.com>
2022-02-09 17:11:06 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# vscode-extension-git-plus
-A copy of Atom git-plus package but for VS Code
+### vscode-extension-git-plus
+A copy of Atom git-plus package but for VS Code.
+
+This project is inspired by two existing projects.
+
+##### Purpose
+The purpose is to provide easy to use Git commands by concatenating multiple ones into a single execution.
+
+##### How to use it
+After you install the package in your Visual Studio Code open up the command palette `Ctrl+Shift+P` on Linux/Windows or `Command+Shift+P` on Mac and start typing `git+`
+
+##### Available commands
+|Name|Git equivalent|
+|-|-|
+|Git+ Add |`git add <current-file>`|
+|Git+ Add All | `git add .` |
+|Git+ Add And Commit | `git add <current-file> && git commit -m <message>` |
+|Git+ Add All And Commit | `git add . && git commit -m <message>`|
+|Git+ Add And Commit And Push | `git add <current-file> && git commit <message> -m && git push` |
+|Git+ Add All And Commit And Push | `git add . && git commit -m <message> && git push` |
+
+##### Inspired by:
+
+- [Atom git-plus package](https://github.com/akonwi/git-plus)
+- [Visual Studio Code git-add-commit-push](https://github.com/alfredbirk/vscode-extension-git-add-commit-push)
---
+++ extension.js
@@ -0,0 +1,70 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+
+function activate(context) {
+
+  console.log('Congratulations, your extension "git-plus" is now active!');
+
+  let GPgitAdd = vscode.commands.registerCommand('extension.GPgitAdd', function() {
+    return vscode.commands.executeCommand("git.stage").then(() => {
+      vscode.window.showInformationMessage('Changes added!');
+    });
+  });
+
+  let GPgitAddAll = vscode.commands.registerCommand('extension.GPgitAddAll', function() {
+    return vscode.commands.executeCommand("git.stageAll").then(() => {
+      vscode.window.showInformationMessage('All changes added!');
+    });
+  });
+
+  let GPgitAddAndCommit = vscode.commands.registerCommand('extension.GPgitAddAndCommit', function() {
+    return vscode.commands.executeCommand("git.stage").then(() => {
+      return vscode.commands.executeCommand("git.commitStaged").then(() => {
+        vscode.window.showInformationMessage('Changes added and commited!');
+      });
+    });
+  });
+
+  let GPgitAddAllAndCommit = vscode.commands.registerCommand('extension.GPgitAddAllAndCommit', function() {
+    return vscode.commands.executeCommand("git.stageAll").then(() => {
+      return vscode.commands.executeCommand("git.commitStaged").then(() => {
+        vscode.window.showInformationMessage('All changes added and commited!');
+      });
+    });
+  });
+
+  let GPgitAddAndCommitAndPush = vscode.commands.registerCommand('extension.GPgitAddAndCommitAndPush', function() {
+    return vscode.commands.executeCommand("git.stage").then(() => {
+      return vscode.commands.executeCommand("git.commitStaged").then(() => {
+        return vscode.commands.executeCommand("git.sync").then(() => {
+          vscode.window.showInformationMessage('Changes pushed!');
+        })
+      });
+    });
+  });
+
+  let GPgitAddAllAndCommitAndPush = vscode.commands.registerCommand('extension.GPgitAddAllAndCommitAndPush', function() {
+    return vscode.commands.executeCommand("git.stageAll").then(() => {
+      return vscode.commands.executeCommand("git.commitStaged").then(() => {
+        return vscode.commands.executeCommand("git.sync").then(() => {
+          vscode.window.showInformationMessage('All changes pushed!');
+        })
+      });
+    });
+  });
+
+  context.subscriptions.push(GPgitAdd, GPgitAddAll, GPgitAddAndCommit, GPgitAddAllAndCommit, GPgitAddAndCommitAndPush, GPgitAddAllAndCommitAndPush);
+
+}
+exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+}
