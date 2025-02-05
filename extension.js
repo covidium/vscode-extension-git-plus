1561e952810d17a0b08ac53c3ddc8ad9993bd9ef
Ovi Calbajos <ocalbajos@tekmetric.com>
2025-02-05 15:27:33 +0200

    [TAK-123]

--- extension.js
+++ extension.js
@@ -38,9 +38,7 @@ function activate(context) {
         let repo = git.repositories[0];
         let ticket = getTicketPrefix();
         // Only set the prefix if there isn't already a message.
-        if (!repo.inputBox.value) {
-            repo.inputBox.value = ticket;
-        } else if (!repo.inputBox.value.startsWith(ticket)) {
+        if (!repo.inputBox.value.startsWith(ticket)) {
             repo.inputBox.value = ticket + repo.inputBox.value;
         }
     }
